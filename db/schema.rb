bf50a4fdd20007bdb55a7ed172a4f395d72bd27a
Jaimelll <jaduran0102@gmail.com>
2020-02-09 07:06:03 -0800

    a postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_02_09_132028) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "locations", force: :cascade do |t|
     t.float "latitude"
     t.float "longitude"
